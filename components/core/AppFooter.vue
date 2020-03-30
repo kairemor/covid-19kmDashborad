8e825012f24099190651be4c647a78e56df11fa4
kmtc44 <kairmor@yahoo.com>
2020-03-30 15:29:53 +0000

    pwa adding

--- components/core/AppFooter.vue
+++ components/core/AppFooter.vue
@@ -1,30 +1,19 @@
 <template>
-  <v-footer
-    id="core-footer"
-    absolute
-    height="82"
-  >
+  <v-footer id="core-footer" absolute height="82">
     <div class="footer-items">
-      <span
-        v-for="link in links"
-        :key="link.name"
-      >
-        <a
-          :href="link.Link"
-          class="tertiary--text footer-links">{{ link.name }}</a>
+      <span v-for="link in links" :key="link.name">
+        <a :href="link.Link" class="tertiary--text footer-links">{{ link.name }}</a>
       </span>
     </div>
-    <v-spacer/>
+    <v-spacer />
     <span class="font-weight-light copyright">
       &copy;
       {{ (new Date()).getFullYear() }}
       <a
         href="https://www.creative-tim.com/"
-        target="_blank">Creative Tim</a>, made with
-      <v-icon
-        color="tertiary"
-        size="17">mdi-heart</v-icon>
-      for a better web
+        target="_blank"
+      >KMTC</a>, made with
+      <v-icon color="tertiary" size="17">mdi-heart</v-icon>for covid19 news
     </span>
   </v-footer>
 </template>
@@ -32,14 +21,9 @@
 <script>
 export default {
   data: () => ({
-    links: [
-      { name: 'Dashboard', Link: '/dashboard' },
-      { name: 'Creative Tim', Link: 'https://www.creative-tim.com' },
-      { name: 'About Us', Link: 'https://creative-tim.com/presentation' },
-      { name: 'Blog', Link: 'https://blog.creative-tim.com' }
-    ]
+    links: [{ name: "Dashboard", Link: "/" }]
   })
-}
+};
 </script>
 
 <style>
